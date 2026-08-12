f1cec9b26e3680b70c23e22ae0d7e4ce29bb02e1
Joaquimfl8 <256233831+JoaFL@users.noreply.github.com>
2026-08-12 16:35:42 -0300

    jihgf

--- src/pages/Catalog/index.jsx
+++ src/pages/Catalog/index.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import listaProdutos from "../../data/produtos";
 import Card from "../../components/Card";
-import { Link } from "react-router-dom";
 import styles from "./Catalog.module.css"
 
 function Catalog() {
@@ -23,16 +22,15 @@ function Catalog() {
 
             <div className={styles.products}>
                 {filtrados.map((p) => (
-                    <Link key={p.id} className={styles.product} to={`/Catalog/${p.id}`}>
-                        <Card
-                            id={p.id}
-                            title={p.nome}
-                            price={p.preco}
-                            description={p.desc}
-                            img={p.imagem}
-                            stock={p.em_estoque}
-                        />
-                    </Link>
+                    <Card
+                        className={styles.product}
+                        id={p.id}
+                        title={p.nome}
+                        price={p.preco}
+                        description={p.desc}
+                        img={p.imagem}
+                        stock={p.em_estoque}
+                    />
                 ))}
             </div>
         </section>
