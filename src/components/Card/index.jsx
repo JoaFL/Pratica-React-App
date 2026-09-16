5cfd7451f783463d86a82f33c5b296c14637ea60
JoaFL <florianojoaquim2008@gmail.com>
2026-09-16 15:39:03 -0300

    Update index.jsx

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -5,16 +5,13 @@ import { formatarPreco } from "../../utils/formatarPreco"
 
 export default function Card({ id, img, title, description, stock, price }) {
     return <>
-
         <Link to={`/Catalog/${id}`} className={styles.link}>
             <div className={styles.card}>
-
                 <img className={styles.img} src={img} alt={title} />
                 <Button className={styles.price} type="primary">{formatarPreco(price)}</Button>
                 <h4 className={styles.title}>{title}</h4>
                 <p className={styles.description}>{description}</p>
             </div>
         </Link>
-
     </>
 }
